5101e5aae997957977e6cf8c60166f193063c8f5
pankaroll <karszo001@pbs.edu.pl>
2024-07-17 00:19:29 +0200

    tescik

---
+++ game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Karol on 17/07/2024.
+//
+
+#include "game.h"
---
+++ game.h
@@ -0,0 +1,14 @@
+//
+// Created by Karol on 17/07/2024.
+//
+
+#ifndef PIXELJUMP_GAME_H
+#define PIXELJUMP_GAME_H
+
+
+class game {
+
+};
+
+
+#endif //PIXELJUMP_GAME_H
