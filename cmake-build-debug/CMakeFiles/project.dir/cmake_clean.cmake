855e7add1dc8db4058a9cc1fcc16f03afd2a00f7
pankaroll <karszo001@pbs.edu.pl>
2024-07-17 01:02:42 +0200

    testv3

---
+++ cmake-build-debug/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/main.cpp.o"
+  "CMakeFiles/project.dir/main.cpp.o.d"
+  "CMakeFiles/project.dir/player.cpp.o"
+  "CMakeFiles/project.dir/player.cpp.o.d"
+  "project"
+  "project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
